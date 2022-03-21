608ec0976cacff45ddc0cc0964d1cd0394d163a9
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-03-21 22:11:47 +0100

    #2 added

--- components, jsx, render/intro/src/App.js
+++ components, jsx, render/intro/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+const userInput = prompt('Podaj swój wiek')
 
-function App() {
+const App = () => {
+  const firstElement = <div>Ciekawe czy to działa?</div>;
+  const anotherElement = <p>Motyla noga, to działa!</p>;
+  const noJsx = "Tekst jakiś:";
+  const expression = (Math.random() + 10).toFixed(2);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Zaczynamy...
+      {firstElement}
+      {anotherElement}
+      <div>
+        <p>{noJsx}</p>
+        {userInput ? <p>{userInput}</p> : <p>Nie podano wartości</p>}
+        <p>{expression}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
