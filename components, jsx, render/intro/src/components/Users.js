214ed0b98be709114346a4d4654a29cbf63c94e9
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-03-24 15:12:46 +0100

    #10 added

--- components, jsx, render/intro/src/components/Users.js
+++ components, jsx, render/intro/src/components/Users.js
@@ -2,10 +2,9 @@ import { data as users } from "../data";
 
 export const Users = () => {
     return users.map(user => (
-        <div key={user.id}>
+        <div>
             <h2>{user.firstName}</h2>
-            <a href ={`mailto: ${user.email}`}>{user.email}</a>
+            <a href= {`mailto: ${user.email}`}>{user.email}</a>
         </div>
     ))
-    
 }
